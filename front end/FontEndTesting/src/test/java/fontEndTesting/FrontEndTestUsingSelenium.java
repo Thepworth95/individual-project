d4840856270b17ba5ebe04879fc6b84f5c53be36
Thepworth95 <Thomas.hepworth@academytrainee.com>
2019-11-19 18:31:37 +0000

    final commit before presentation

--- front end/FontEndTesting/src/test/java/fontEndTesting/FrontEndTestUsingSelenium.java
+++ front end/FontEndTesting/src/test/java/fontEndTesting/FrontEndTestUsingSelenium.java
@@ -18,7 +18,7 @@ public class FrontEndTestUsingSelenium {
 	}
 	
 	@Test
-	public void test1() throws InterruptedException {
+	public void UserStoryTest() throws InterruptedException {
 		driver.get("file:///c%3A/Users/Admin/individual%20project/front%20end/index.html");
 		WebElement webEle = driver.findElement(By.xpath("/html/body/nav/a[4]"));
 		webEle.click();
@@ -62,8 +62,7 @@ public class FrontEndTestUsingSelenium {
 		WebElement loginsubmitdetails2 = driver.findElement(By.xpath("/html/body/form/button"));
 		loginsubmitdetails2.click();
 		WebElement deleteuser = driver.findElement(By.xpath("/html/body/button"));
-		deleteuser.click();
-		
+		deleteuser.click();	
 		WebElement loginuserame3 = driver.findElement(By.xpath("/html/body/form/input[1]"));
 		loginuserame3.sendKeys("zak");
 		WebElement loginpassword3 = driver.findElement(By.xpath("/html/body/form/input[2]"));
@@ -75,7 +74,42 @@ public class FrontEndTestUsingSelenium {
 		driver.switchTo().alert().accept(); 
 		assertEquals("", "username does not exist", a);
 	}
+	
+	@Test
+	public void loginTest() throws InterruptedException {
+		driver.get("file:///c%3A/Users/Admin/individual%20project/front%20end/login.html");
+		WebElement loginuserame = driver.findElement(By.xpath("/html/body/form/input[1]"));
+		loginuserame.sendKeys("tom");
+		WebElement loginpassword = driver.findElement(By.xpath("/html/body/form/input[2]"));
+		loginpassword.sendKeys("password");
+		WebElement loginsubmitdetails = driver.findElement(By.xpath("/html/body/form/button"));
+		loginsubmitdetails.click();
+		Thread.sleep(5000);	
+		String a = driver.findElement(By.xpath("/html/body/div/h1")).getText();
+		System.out.println(a);
+		assertEquals("", "tom-Shulk", a);		
+	}
 		
+	@Test
+	public void registerTest() throws InterruptedException {
+		driver.get("file:///c%3A/Users/Admin/individual%20project/front%20end/register.html");
+		WebElement createuserame = driver.findElement(By.xpath("/html/body/form/input[1]"));
+		createuserame.sendKeys("andy");
+		WebElement createpassword = driver.findElement(By.xpath("/html/body/form/input[2]"));
+		createpassword.sendKeys("password");
+		WebElement createfirstname = driver.findElement(By.xpath("/html/body/form/input[3]"));
+		createfirstname.sendKeys("frenchy");
+		WebElement createlastname = driver.findElement(By.xpath("/html/body/form/input[4]"));
+		createlastname.sendKeys("lastname");
+		WebElement createfavchar = driver.findElement(By.xpath("/html/body/form/input[5]"));
+		createfavchar.sendKeys("57");
+		WebElement createregister = driver.findElement(By.xpath("/html/body/form/button"));
+		createregister.click();
+		Thread.sleep(5000);	
+		String a = driver.findElement(By.xpath("/html/body/div/h1")).getText();
+		assertEquals("", "andy-Shulk", a);		
+	}
+	
 	
 	@After
 	public void destroy() {
